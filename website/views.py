bb6afbee477dca9659024800729dd54c14a33ca6
stman3 <75269916+stman3@users.noreply.github.com>
2023-07-05 15:02:18 +0300

    finish the choice and add some style.

--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template,request
+from flask import Blueprint, render_template,request,session
+
 import praw
 import requests
 
@@ -23,10 +24,28 @@ def classic():
         for post in hot_posts:
             posts_data.append({'url': post.url, 'ups': post.ups})
 
-    current_index = 0
+    current_index = session.get('current_index', 0)
     if request.method == 'POST':
-        current_index = int(request.form.get('current_index', 0))
-        if current_index < len(posts_data) - 1:
-            current_index += 1
+        current_index = session['current_index']
+        choice = request.form.get('choice')
+        if choice == 'higher':
+            print('here',current_index)
+            print(posts_data[0]['ups'])
+            if int(posts_data[current_index]['ups']) < int(posts_data[current_index+1]['ups']):
+                print(current_index)
+                current_index += 1
+                session['current_index'] = current_index
+            else:
+                pass
 
+        elif choice == 'lower':
+            print(len(posts_data))
+            print(current_index)
+            if int(posts_data[current_index]['ups']) > int(posts_data[current_index+1]['ups']):
+                current_index += 1
+                session['current_index'] = current_index
+                print(current_index)
+            else:
+                pass
+    session['current_index'] = current_index
     return render_template("classic.html", posts=posts_data, current_index=current_index)
